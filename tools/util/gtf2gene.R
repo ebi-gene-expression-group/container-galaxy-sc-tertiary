06283e92907321289efca757b97c46c3bd94a2ea
Jonathan Manning <jmanning@ebi.ac.uk>
2019-10-18 14:41:55 +0100

    Add script for extracting gene annotation table from GTF (#94)
    * Added a simple script to produce a gene annotation table with mitochondrial genes flagged
    
    * Fixed author
    
    * Properly parameterise script for future-proofing
    
    * Remove older basic script
    
    * Add test data
    
    * Modify shed entry
    
    * gene -> feature to reflect generic feature extraction
    
    * Remove unnecessary Rscript call
    
    * Restore original tool ID
    
    * Extend list of default possible mitochondrial chromosome names
    
    * Further modification to add additional function of FASTA file synchronisation
    
    * Remove test line
    
    * Fix typo
    
    * Bump version

--- tools/util/gtf2gene.R
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env Rscript
-
-suppressPackageStartupMessages(require(rtracklayer))
-args <- commandArgs(TRUE)
-
-annotation <- elementMetadata(import( args[1] ))
-genes <- unique(annotation[['gene_id']])
-writeLines(genes[ ! is.na(genes)], con = 'genes.txt')
