5d3528442f6bb7c63f96f88dff53eb0209a8df22
Pablo Moreno <pcm32@users.noreply.github.com>
2020-10-26 16:26:30 +0000

    Supplementary materials paper review for subs (#204)
    * cli URL fixes
    
    * Case fixing and none cli
    
    * All links working, besides missing tools now added to HCA and SCXA instances.
    
    * Minor line update
    
    * Workflow screenshot for supplemental
    
    FASTQ - Alevin - Scanpy
    
    * Adding a workflow to the supplemental information
    
    I don't think the image is working yet
    
    * Update sup_note_workflows.md
    
    Tiniest of changes of a capital letter for consistency.
    
    * Fix image in workflows markdown
    
    * Corrections on workflows supp
    
    * Supplementary materials and helm config.
    
    * Working version
    
    * Fix links on all supplementary materials.
    
    * Cherry pick + resolve to make test pass.
    
    * Reduce file size
    
    Co-authored-by: Wendi Bacon <44605769+nomadscientist@users.noreply.github.com>

--- czi_tables/create_table_S2.sh
+++ czi_tables/create_table_S2.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
 write_rows_for_modules() {
-  local cli_link="[$cli](https://github.com/ebi-gene-expression-group/$cli/tree/master)"
+  if [[ $cli != "None" ]]; then
+    local cli_link="[$cli](https://github.com/ebi-gene-expression-group/$cli/tree/master)"
+  else
+    local cli_link="None"
+  fi
   for mod in $(ls $path/*.xml | grep -v macro); do
     local desc=$(xpath -q -e /tool/description $mod | sed -E 's+</?description>++g')
     local mod_id=$(xpath -q -e /tool/@id $mod | sed -E 's+(id=)?"++g' | sed 's/ *//g')
-    local mod_instance_link=https://humancellatlas.usegalaxy.eu/tool_runner?tool_id=toolshed.g2.bx.psu.edu%2Frepos%2Febi-gxa%2F$mod_id%2F$mod_id
-    local mod_ts_link=https://toolshed.g2.bx.psu.edu/view/ebi-gxa/$mod_id
-    echo "| [$mod_id]($mod_instance_link)<sup>[TS]($mod_ts_link) | $desc | [$cli]($cli_link) | ${mappings[$mod_id]} |" >> S2_modules_aut.md
+    local mod_id_lc=$(echo $mod_id  | tr '[:upper:]' '[:lower:]')
+    local mod_instance_link=https://humancellatlas.usegalaxy.eu/tool_runner?tool_id=toolshed.g2.bx.psu.edu%2Frepos%2Febi-gxa%2F$mod_id_lc%2F$mod_id
+    local mod_ts_link=https://toolshed.g2.bx.psu.edu/view/ebi-gxa/$mod_id_lc
+    echo "| [$mod_id]($mod_instance_link)<sup>[TS]($mod_ts_link) | $desc | $cli_link | ${mappings[$mod_id]} |" >> S2_modules_aut.md
   done
 }
 
@@ -32,12 +37,12 @@ write_rows_for_modules
 
 # Seurat
 path=$tertiary_path/seurat
-cli=seurat-scripts
+cli=r-seurat-scripts
 write_rows_for_modules
 
 # SC3
 path=$tertiary_path/sc3
-cli=sc3-scripts
+cli=bioconductor-sc3-scripts
 write_rows_for_modules
 
 # scMap
@@ -47,7 +52,7 @@ write_rows_for_modules
 
 # scater
 path=$tertiary_path/scater
-cli=scater-scripts
+cli=bioconductor-scater-scripts
 write_rows_for_modules
 
 # Monocle3
@@ -81,7 +86,7 @@ cli=None
 write_rows_for_modules
 
 # droplet-utils
-path=$tertiray_path/dropletutils
+path=$tertiary_path/dropletutils
 cli=dropletutils-scripts
 write_rows_for_modules
 
