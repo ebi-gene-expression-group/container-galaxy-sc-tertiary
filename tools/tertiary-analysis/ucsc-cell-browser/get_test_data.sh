69cc8188fed77a8f32e2ff250faadcf99cca70c7
Pablo Moreno <pablo.a.moreno@gmail.com>
2021-04-18 00:32:44 +0100

    wget agressive retrials

--- tools/tertiary-analysis/ucsc-cell-browser/get_test_data.sh
+++ tools/tertiary-analysis/ucsc-cell-browser/get_test_data.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
-wget http://ftp.ebi.ac.uk/pub/databases/microarray/data/atlas/sc_experiments/E-MTAB-6077/E-MTAB-6077.project.h5ad
+wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 3 http://ftp.ebi.ac.uk/pub/databases/microarray/data/atlas/sc_experiments/E-MTAB-6077/E-MTAB-6077.project.h5ad
 mv E-MTAB-6077.project.h5ad test-data/
