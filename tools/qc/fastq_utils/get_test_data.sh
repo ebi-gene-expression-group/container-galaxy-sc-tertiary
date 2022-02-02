2a0db33e60a00e0d947f9576074ba8b5b11270fe
Iris Yu <irisyu@ebi.ac.uk>
2022-02-02 16:37:07 +0800

    Edit link in get_test_data.sh

--- tools/qc/fastq_utils/get_test_data.sh
+++ tools/qc/fastq_utils/get_test_data.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
-BASE_LINK='https://github.com/nunofonseca/fastq_utils/tree/master/tests'
+BASE_LINK="https://raw.githubusercontent.com/nunofonseca/fastq_utils/tree/master/tests"
 
 BAR11_FILE="barcode_test_1.fastq.gz"
 BAR12_FILE="barcode_test_2.fastq.gz"
 BAR21_FILE="barcode_test2_1.fastq.gz"
 BAR22_FILE="barcode_test2_2.fastq.gz"
+INTER_FILE="inter.fastq.gz"
 
 BAR11_LINK=$BASE_LINK"/"$BAR11_FILE
 BAR12_LINK=$BASE_LINK"/"$BAR12_FILE
 BAR21_LINK=$BASE_LINK"/"$BAR21_FILE
 BAR22_LINK=$BASE_LINK"/"$BAR22_FILE
+INTER_LINK=$BASE_LINK"/"$INTER_FILE
 
 function get_data {
   local link=$1
@@ -29,3 +31,4 @@ get_data $BAR11_LINK $BAR11_FILE
 get_data $BAR12_LINK $BAR12_FILE
 get_data $BAR21_LINK $BAR12_FILE
 get_data $BAR22_LINK $BAR22_FILE
+get_data $INTER_LINK $INTER_FILE
