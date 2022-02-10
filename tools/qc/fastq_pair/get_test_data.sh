6e79c6f290f4670329710f767bacf0b36ec0de63
Iris Yu <irisyu@ebi.ac.uk>
2022-02-10 18:03:32 +0800

    Add script to fetch test data

---
+++ tools/qc/fastq_pair/get_test_data.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+BASE_LINK="https://raw.githubusercontent.com/linsalrob/fastq-pair/master/test"
+
+LEFT_FILE="left.fastq"
+RIGHT_FILE="right.fastq"
+
+LEFT_LINK=$BASE_LINK"/"$LEFT_FILE
+RIGHT_LINK=$BASE_LINK"/"$RIGHT_FILE
+
+function get_data {
+  local link=$1
+  local fname=$2
+
+  if [ ! -f $fname ]; then
+    echo "$fname not available locally, downloading.."
+    wget -O $fname --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 3 $link
+  fi
+}
+
+# Get test data
+pushd test-data
+
+get_data $LEFT_LINK $LEFT_FILE
+get_data $RIGHT_LINK $RIGHT_FILE
