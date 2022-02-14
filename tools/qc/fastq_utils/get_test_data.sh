9736de4a77c6b0c9ec80299154100de03165a211
Iris Yu <irisyu@ebi.ac.uk>
2022-02-14 15:57:57 +0800

    Add script to fetch test data

--- tools/qc/fastq_utils/get_test_data.sh
+++ tools/qc/fastq_utils/get_test_data.sh
@@ -7,12 +7,18 @@ BAR12_FILE="barcode_test_2.fastq.gz"
 BAR21_FILE="barcode_test2_1.fastq.gz"
 BAR22_FILE="barcode_test2_2.fastq.gz"
 INTER_FILE="inter.fastq.gz"
+A1_FILE="a_1.fastq.gz"
+POLYAT_FILE="poly_at.fastq.gz"
+POLYAT3_FILE="poly_at_len3.fastq.gz"
 
 BAR11_LINK=$BASE_LINK"/"$BAR11_FILE
 BAR12_LINK=$BASE_LINK"/"$BAR12_FILE
 BAR21_LINK=$BASE_LINK"/"$BAR21_FILE
 BAR22_LINK=$BASE_LINK"/"$BAR22_FILE
 INTER_LINK=$BASE_LINK"/"$INTER_FILE
+A1_LINK=$BASE_LINK"/"$A1_FILE
+POLYAT_LINK=$BASE_LINK"/"$POLYAT_FILE
+POLYAT3_LINK=$BASE_LINK"/"$POLYAT3_FILE
 
 function get_data {
   local link=$1
@@ -32,3 +38,6 @@ get_data $BAR12_LINK $BAR12_FILE
 get_data $BAR21_LINK $BAR21_FILE
 get_data $BAR22_LINK $BAR22_FILE
 get_data $INTER_LINK $INTER_FILE
+get_data $A1_LINK $A1_FILE
+get_data $POLYAT_LINK $POLYAT_FILE
+get_data $POLYAT3_LINK $POLYAT3_FILE
