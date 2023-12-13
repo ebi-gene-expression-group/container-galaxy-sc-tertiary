38a657fff894c79e97036e3417daf29982acaab5
anilthanki <thanki.anil@gmail.com>
2023-12-13 10:48:26 +0000

    merges mlm files into one

--- tools/tertiary-analysis/decoupler/decoupler_pathway_inference.py
+++ tools/tertiary-analysis/decoupler/decoupler_pathway_inference.py
@@ -95,7 +95,7 @@ dc.run_mlm(
 
 if args.output is not None:
     # write adata.obsm[mlm_key] and adata.obsm[mlm_pvals_key] to the output network files
-    adata.obsm[["mlm_estimate", "mlm_pvals"]].to_csv(args.output + "_mlm.tsv", sep="\t")
+    adata.obsm["mlm_estimate", "mlm_pvals"].to_csv(args.output + "_mlm.tsv", sep="\t")
 
 # if args.activities_path is specified, generate the activities AnnData and save the AnnData object to the specified path
 if args.activities_path is not None:
